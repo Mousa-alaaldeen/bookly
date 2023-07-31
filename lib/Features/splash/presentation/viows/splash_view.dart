21a4851d1cbf5f4b514b6b4dd25651a80db32455
alaaldee <musa.alaaldeen77@gmail.com>
2023-07-31 14:06:03 +0300

    initial commit

---
+++ lib/Features/splash/presentation/viows/splash_view.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:bookly/Features/splash/presentation/viows/widget/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SplaShView extends StatelessWidget {
+  const SplaShView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SplaShViewBody(),
+    );
+  }
+}
