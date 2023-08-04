ee66df9fee863c22dc62c80af4b702845bfa79d5
alaaldee <musa.alaaldeen77@gmail.com>
2023-08-04 17:56:31 +0300

    book entity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,10 @@
+// بتعبر عن الداتا اللي بدها تنعرض ب 
+// ui
+class BookEntity {
+  final String image;
+  final String title;
+  final String authorName;
+  final num price;
+  final num raning;
+  BookEntity(this.authorName, this.title, this.image, this.price, this.raning);
+}
