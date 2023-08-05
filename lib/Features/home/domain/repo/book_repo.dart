169c5c77759a101a55edb8a3dbe8856f9bed93df
alaaldee <musa.alaaldeen77@gmail.com>
2023-08-05 15:12:28 +0300

    refactor to use cases class

--- lib/Features/home/domain/repo/book_repo.dart
+++ lib/Features/home/domain/repo/book_repo.dart
@@ -1,7 +1,8 @@
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/core/errors/failere.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Fail, List<BookEntity>>> featchFeatureBooks();
-  Future<Either<Fail, List<BookEntity>>> featchNewesBooks();
+  Future<Either<Failure, List<BookEntity>>> featchFeatureBooks();
+  Future<Either<Failure, List<BookEntity>>> featchNewesBooks();
 }
--- lib/Features/home/domain/use_cases/featch_feature_books_case.dart
+++ lib/Features/home/domain/use_cases/featch_feature_books_case.dart
@@ -1,13 +1,18 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:dartz/dartz.dart';
 
-import '../entities/book_entity.dart';
+import '../../../../core/errors/failere.dart';
+import '../../../../core/use_cases/use_cases.dart';
 import '../repo/book_repo.dart';
 
-class FeatchFeatureBooksUseCase {
+class FeatchFeatureBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
   FeatchFeatureBooksUseCase(this.homeRepo);
-  Future<Either<Fail, List<BookEntity>>> featchFeatureBooks() {
-    //check permssion
+  
+  @override
+  Future<Either< Failure, List<BookEntity>>> call([NoParam? param]) {
     return homeRepo.featchFeatureBooks();
   }
 }
+
+
