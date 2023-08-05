30e39ca9787aa9a8931e561f962171b7a3190fa1
alaaldee <musa.alaaldeen77@gmail.com>
2023-08-05 15:20:19 +0300

    fetch newse books use case

---
+++ lib/Features/home/domain/use_cases/faetch_newes_books.dart
@@ -0,0 +1,18 @@
+import 'package:bookly/Features/home/domain/repo/book_repo.dart';
+import 'package:bookly/core/use_cases/use_cases.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failere.dart';
+import '../entities/book_entity.dart';
+
+class FaerchNewseBooks extends UseCase<List<BookEntity>, NoParam> {
+  final HomeRepo homeRepo;
+
+  FaerchNewseBooks(this.homeRepo);
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) {
+
+    return homeRepo.featchNewesBooks();
+  }
+}
