33f2d5a88c68a8e099c03d47922ce59635ace28e
alaaldee <musa.alaaldeen77@gmail.com>
2023-08-06 18:47:11 +0300

    create featcherd books states

---
+++ lib/Features/home/presentaion/manger/featcherd_books_cubit/featcherd_books_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:cubit_form/cubit_form.dart';
+
+import '../featcherd_books_state.dart';
+
+class FeatcherdBooksCubit extends Cubit {
+  FeatcherdBooksCubit() : super(FeatureBooksInitial());
+}
