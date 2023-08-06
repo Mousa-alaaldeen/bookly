1e50c445a3abc74c48064ee05b7b747b3f458879
alaaldee <musa.alaaldeen77@gmail.com>
2023-08-06 20:30:49 +0300

    implemint cubit  featchFeatureBooks

--- lib/Features/home/presentaion/manger/featcherd_books_cubit/featcherd_books_cubit.dart
+++ lib/Features/home/presentaion/manger/featcherd_books_cubit/featcherd_books_cubit.dart
@@ -1,7 +1,19 @@
+import 'package:bookly/Features/home/domain/use_cases/featch_feature_books_case.dart';
 import 'package:cubit_form/cubit_form.dart';
 
 import '../featcherd_books_state.dart';
 
 class FeatcherdBooksCubit extends Cubit {
-  FeatcherdBooksCubit() : super(FeatureBooksInitial());
+  FeatcherdBooksCubit(this.featchFeatureBooksUseCase)
+      : super(FeatureBooksInitial());
+  final FeatchFeatureBooksUseCase featchFeatureBooksUseCase;
+  Future<void> featchFeatureBooks() async {
+    emit(FeatureBooksLoding());
+    var result = await featchFeatureBooksUseCase.call();
+    result.fold((failure) {
+      emit(FeatureBooksError(failure.massage));
+    }, (books) {
+      emit(FeatureBooksSuccess(books));
+    });
+  }
 }
